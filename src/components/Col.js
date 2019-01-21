2aa128c0cdf28bc290898656e51cb6d1b1a21cb6
drd-2rcl <drd.rclrd@gmail.com>
2019-01-21 19:27:27 -0200

    first commit

---
+++ src/components/Col.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Col = styled.div`
+display: flex;
+flex-direction: column;
+`;
+
+export default () => (
+  <Col />  
+);
---
+++ src/components/input.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Col from '../components/Col';
+
+const Input = styled.input`
+width: 100%;
+padding: 12px 20px;
+margin: 8px 0;
+display: inline-block;
+border: 1px solid #ccc;
+border-radius: 4px;
+box-sizing: border-box;
+`;
+
+export default () => (
+    <Input />
+);
